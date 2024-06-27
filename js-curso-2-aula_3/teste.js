2e676aa7c6423840741be742f8d9da921423a1d1
Alvo7 <102003171+Alvo7@users.noreply.github.com>
2024-06-27 20:48:55 -0300

    Add files via upload

---
+++ js-curso-2-aula_3/teste.js
@@ -0,0 +1,13 @@
+//let frutas = ["Maçã", "Uva", "Laranja"];
+//console.log(frutas[0]); // Saída: "Maçã"
+//console.log(frutas[2]); // Saída: "Laranja")
+//frutas.push("Morango");
+//console.log(frutas); // Saída: ["Maçã", "Uva", "Laranja", "Morango"]
+//frutas.pop();
+//console.log(frutas); // Saída: ["Maçã", "Uva", "Laranja"]
+
+//criando um array do zero, sempre utilizar colchetes []
+let listaGenerica = [];
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(linguagensDeProgramacao[0], linguagensDeProgramacao[1], linguagensDeProgramacao[2]);
